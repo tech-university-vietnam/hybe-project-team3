745136eb1dee6b7bbf9554794be9dcd7f371c1dd
ed-phan <edward_phan@mckinsey.com>
2022-08-17 18:55:33 +0700

    initial call to BackEnd API
    - add axios library for async operation to make a HTTP get call to BackEnd api
    - allow access-control-allow-origin to make call to `localhost:8088` port from port 3000

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+const axios = require("axios");
 
 function App() {
+  const [data, setData] = useState("");
+  const url = "http://localhost:8088/api/day";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(url);
+      console.log("response is", response);
+      setData(response.data.day);
+    };
+    fetchData();
+  }, [data]);
+
   return (
     <div className="App">
       <p>HYBE app page</p>
+      <p>{data}</p>
     </div>
   );
 }
