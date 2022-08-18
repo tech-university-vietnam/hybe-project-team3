53e0082d90d1706489ca87869bd005a724c76e9d
bao-qnguyen97 <111408781+bao-qnguyen97@users.noreply.github.com>
2022-08-18 12:34:09 +0700

    Add Test, Pylint, Project structure for BE (#7)
    * Add Test, Pylint
    
    * Add workflows
    
    * Fix trigger rule
    
    * add Backend build
    
    * Update name
    
    * Remove name
    
    * Update version
    
    * Bump version
    
    * update version
    
    * test bad pylint
    
    * Test new workflow
    
    * Update path
    
    * Update path
    
    * Fix test
    
    * Update rule
    
    * Update rule
    
    * Fix lint
    
    * rename folder
    
    * add docker image

---
+++ BE/tests/test_example.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from unittest.mock import patch, Mock
+
+import unittest
+
+from app.main import get_day_of_week
+
+
+class TestGetDayOfWeek(unittest.TestCase):
+    def test_get_day_of_week(self):
+        dow = datetime.now().strftime("%A")
+        assert dow == get_day_of_week()['day']
+
+    @patch('app.main.get_datetime_now')
+    def test_mocked_get_day_of_week(self, mock_get_dow: Mock):
+        # Assign new value to force now() return same result
+        mock_get_dow.return_value = datetime(2022, 8, 7)
+        expected = 'Sunday'
+        self.assertEqual(expected, get_day_of_week()['day'])
