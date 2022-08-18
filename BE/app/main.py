078284c54a75d7f0d207e4892d46b2a58b5f9c0d
bao-qnguyen97 <111408781+bao-qnguyen97@users.noreply.github.com>
2022-08-18 10:26:53 +0700

    Add Test, Pylint

--- BE/app/main.py
+++ BE/app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from datetime import datetime
 
+
+def get_datetime_now() -> datetime:
+    # Use arrow function here to mock the builtin library
+    return datetime.now()
+
+
 app = FastAPI()
+
 
 @app.get("/day", tags=["Dates"])
 def get_day_of_week():
     """
     Get the current day of week
     """
+
     return {
-        "day": datetime.now().strftime("%A")
+        "day": get_datetime_now().strftime("%A")
     }
