e0e41923004b920ab5fb63894975b49b0fe935a2
bao-qnguyen97 <111408781+bao-qnguyen97@users.noreply.github.com>
2022-08-22 12:40:56 +0700

    [Feat]: Improve FE structure (#8)

---
+++ FE/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Greeting } from 'components/Greeting/Greeting'
+import "./HomePage.css"
+
+export const HomePage = () => (
+  <div className='textAlign'>
+    <Greeting />
+  </div>
+)
+
