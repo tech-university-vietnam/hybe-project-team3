3dc7d4c884b0712511e6d36b5a1b1fb40313c990
ed-phan <edward_phan@mckinsey.com>
2022-08-25 09:18:49 +0700

    add sanitation tests

--- FE/src/components/LoginForm/LoginForm.test.js
+++ FE/src/components/LoginForm/LoginForm.test.js
@@ -1,4 +1,10 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  userEvent,
+  act,
+  fireEvent,
+  render,
+  screen,
+} from "@testing-library/react";
 import LoginForm from "./LoginForm";
 
 describe("LoginForm", () => {
@@ -13,5 +19,71 @@ describe("LoginForm", () => {
     render(<LoginForm />);
 
     expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("shows the async login error when username or password is NOT correct", async () => {
+    render(<LoginForm />);
+
+    const inputEl = screen.getByLabelText(/email/i);
+    const passwordEl = screen.getByLabelText(/password/i);
+
+    fireEvent.change(inputEl, { target: { value: "a@a.com" } });
+    fireEvent.change(passwordEl, { target: { value: "a" } });
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button"));
+    });
+
+    expect(
+      screen.getByText("Cannot login to server. Please try again")
+    ).toBeInTheDocument();
+  });
+
+  it("shows email error when no email is in the email input", async () => {
+    render(<LoginForm />);
+
+    const passwordEl = screen.getByLabelText(/password/i);
+    fireEvent.change(passwordEl, { target: { value: "a" } });
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button"));
+    });
+
+    expect(screen.getByText("Email is required")).toBeInTheDocument();
+  });
+
+  it("shows password error when no password is in the password input", async () => {
+    render(<LoginForm />);
+
+    const inputEl = screen.getByLabelText(/email/i);
+    fireEvent.change(inputEl, { target: { value: "a@a.com" } });
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button"));
+    });
+
+    expect(screen.getByText("Password is required")).toBeInTheDocument();
+  });
+
+  it("shows email and password errors when no value is in both inputs", async () => {
+    render(<LoginForm />);
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button"));
+    });
+    expect(screen.getByText("Email is required")).toBeInTheDocument();
+    expect(screen.getByText("Password is required")).toBeInTheDocument();
+  });
+
+  it("shows error for invalid email format", async () => {
+    render(<LoginForm />);
+
+    const inputEl = screen.getByLabelText(/email/i);
+    fireEvent.change(inputEl, { target: { value: "a" } });
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button"));
+    });
+    expect(screen.getByText("Email is invalid")).toBeInTheDocument();
   });
 });
