6e12bc928617cdd57299b155dff9317bd8719e38
Al3xDo <hoanghuy9998@gmail.com>
2022-08-25 09:22:23 +0700

    update unittest

--- BE/app/main.py
+++ BE/app/main.py
@@ -12,7 +12,6 @@ settings= get_settings()
 app = FastAPI(debug=True)
 
 
-
 def setup(setup_app: FastAPI):
     from app.controllers.auth.auth import router as auth_router
     from app.controllers.user import router as user_router
--- BE/tests/domain/user.py
+++ BE/tests/domain/user.py
@@ -3,7 +3,7 @@ from app.model.user import User
 from datetime import datetime
 
 now = datetime.now()
-class TestBook:
+class TestUser:
     def test_constructor_should_create_instance(self):
         user = User(
             id=1,
