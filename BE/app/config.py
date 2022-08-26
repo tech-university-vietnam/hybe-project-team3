44acda01deafc59501c1a603bb8ca18a0027999f
bao-qnguyen97 <111408781+bao-qnguyen97@users.noreply.github.com>
2022-08-26 13:45:39 +0700

    Fix env file

--- BE/app/config.py
+++ BE/app/config.py
@@ -3,12 +3,19 @@ from functools import lru_cache
 from pydantic import BaseSettings
 
 
+def get_env_file():
+    filename = '.'.join(['.env', os.getenv('ENV', 'local')])
+    env_filepath = os.path.join(os.getcwd(), filename)
+
+    return env_filepath
+
+
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET: str
 
     class Config:
-        env_file = os.path.join(os.getcwd(), '.env')
+        env_file = get_env_file()
         env_file_encoding = 'utf-8'
 
 
