aa69f0722bddea439c7f4f9473dc7600245cb534
bao-qnguyen97 <111408781+bao-qnguyen97@users.noreply.github.com>
2022-08-26 11:16:35 +0700

    Fix flake

--- BE/app/main.py
+++ BE/app/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
 
-
 app = FastAPI(debug=True)
 
 
 def setup(setup_app: FastAPI):
-
     from app.controllers.auth.auth import router as auth_router
     from app.controllers.user.user import router as user_router
     from app.infrastructure.postgresql.database import create_tables
 
     create_tables()
-
 
     setup_app.include_router(auth_router)
     setup_app.include_router(user_router)
