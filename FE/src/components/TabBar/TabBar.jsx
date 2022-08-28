7198483b3be94c508347ebbac24f83da88e2d184
Dung (Yun) Pham <50902849+dungplt242@users.noreply.github.com>
2022-08-28 10:22:44 +0700

    init tab bar

---
+++ FE/src/components/TabBar/TabBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import InventoryIcon from '@mui/icons-material/Inventory'
+import FactCheckIcon from '@mui/icons-material/FactCheck'
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive'
+
+const TabBar = props => {
+    return (
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: '#000000'}}>
+            <nav>
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <InventoryIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Inventory" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <FactCheckIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Wishlist" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <NotificationsActiveIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Inventory" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </nav>
+        </Box>
+    )
+}
+
+TabBar.propTypes = {}
+
+export default TabBar
