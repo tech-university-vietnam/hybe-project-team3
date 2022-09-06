cd7e26286e087f9f8bc36d07cb0832b3fbe74baf
bao-qnguyen97 <111408781+bao-qnguyen97@users.noreply.github.com>
2022-09-06 21:30:08 +0700

    Fix number required

--- BE/app/model/tracking_medicine.py
+++ BE/app/model/tracking_medicine.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class TrackingMedicine(BaseModel):
     id: int
     name: str
-    number: int
+    number: Optional[int]
     status: str
