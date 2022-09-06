aba05bab6acb9801b5a228734da2a75dad66e877
ed-phan <edward_phan@mckinsey.com>
2022-09-06 10:13:50 +0700

    add filter dropdown for track list

--- FE/src/pages/Dashboard/Dashboard.jsx
+++ FE/src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import TabBar from '../../components/TabBar/TabBar'
-import AddItemButton from '../../components/AddItemButton/AddItemButton'
-import './Dashboard.css'
-import MedicineItem from 'components/MedicineItem/MedicineItem'
-import { Button, Pagination } from '@mui/material'
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import TabBar from "../../components/TabBar/TabBar";
+import AddItemButton from "../../components/AddItemButton/AddItemButton";
+import "./Dashboard.css";
+import MedicineItem from "components/MedicineItem/MedicineItem";
+import { Pagination } from "@mui/material";
+import Filter from "components/Filter/Filter";
 
-const Dashboard = props => {
+const Dashboard = (props) => {
   const [page, setPage] = useState(1);
-  const data = Array(10).fill('item');
-  console.log(data)
+  const data = Array(10).fill("item");
+  console.log(data);
+
+  const [selectedStatus, SetSelectedStatus] = useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    SetSelectedStatus(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
 
   return (
     <div className="dashboard-container">
@@ -18,23 +30,34 @@ const Dashboard = props => {
       <div className="content-container">
         <div className="content-header">
           <AddItemButton type="inventory" />
-          <Pagination count={10} variant="outlined" page={page} onChange={(event, pageNumber) => setPage(pageNumber)} />
-          <Button>
-            <FilterAltIcon/>
-            Filter
-          </Button>
+          <Pagination
+            count={10}
+            variant="outlined"
+            page={page}
+            onChange={(event, pageNumber) => setPage(pageNumber)}
+          />
+          <Filter
+            statuses={["LISTED", "NOT LISTED", "FINISHED LISTING"]}
+            selectedStatuses={selectedStatus}
+            handleChange={handleChange}
+          />
         </div>
         <div className="data-container">
-          {
-            data.map((item) => <MedicineItem name='Sinovac' exp='20/10/2030'/>)
-          }
+          {data.map((item) => (
+            <MedicineItem name="Sinovac" exp="20/10/2030" />
+          ))}
         </div>
-        <Pagination count={10} variant="outlined" page={page} onChange={(event, pageNumber) => setPage(pageNumber)} />
+        <Pagination
+          count={10}
+          variant="outlined"
+          page={page}
+          onChange={(event, pageNumber) => setPage(pageNumber)}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-Dashboard.propTypes = {}
+Dashboard.propTypes = {};
 
-export default Dashboard
+export default Dashboard;
