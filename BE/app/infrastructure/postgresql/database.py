7d9cc307fc335f8d2db2f423d69ae9347d1afcbc
bao-qnguyen97 <111408781+bao-qnguyen97@users.noreply.github.com>
2022-09-07 14:01:43 +0700

    Auto seed medicines

--- BE/app/infrastructure/postgresql/database.py
+++ BE/app/infrastructure/postgresql/database.py
@@ -1,3 +1,7 @@
+import json
+import os
+from datetime import datetime
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
@@ -25,3 +29,34 @@ def create_tables():
 
 def drop_tables():
     Base.metadata.drop_all(bind=engine)
+
+
+def seed_medicines():
+    from app.infrastructure.postgresql.tracking_medicine.tracking_medicine import TrackingMedicineDTO
+    from app.domains.medicine.medicine_repository import MedicineStatus
+
+    path = os.getcwd() + '/app/resources/medicines.json'
+    with open(path, 'r') as file:
+        json_meds = json.load(file)
+
+    meds_dto = list(map(lambda med: TrackingMedicineDTO(
+        name=med['name'],
+        status=MedicineStatus.calculate_create_status(datetime.fromisoformat(med['expired_date'])),
+        expired_date=datetime.fromisoformat(med['expired_date']),
+        created_at=datetime.fromisoformat(med['created_at']),
+        created_by=0,
+        hospital_id=0,
+
+    ), json_meds))
+
+    session.add_all(meds_dto)
+    session.commit()
+
+
+def init_database():
+    if os.getenv('ENV', 'local') == "test":
+        drop_tables()
+    create_tables()
+
+    # Seed data
+    seed_medicines()
