a2792d849e4afee2b8c8f9d1b6555f4f2588f6e8
ed-phan <edward_phan@mckinsey.com>
2022-09-07 13:43:10 +0700

    add WishList.jsx

---
+++ FE/src/pages/WishList/WishList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WishList = (props) => {
+  return <div>WishList</div>;
+};
+
+WishList.propTypes = {};
+
+export default WishList;
