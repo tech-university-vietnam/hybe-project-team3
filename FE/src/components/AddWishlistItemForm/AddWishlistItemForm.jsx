b2645af127a954a278d3d3660c839f616fa80b09
Dung (Yun) Pham <50902849+dungplt242@users.noreply.github.com>
2022-09-07 16:38:28 +0700

    added form

--- FE/src/components/AddWishlistItemForm/AddWishlistItemForm.jsx
+++ FE/src/components/AddWishlistItemForm/AddWishlistItemForm.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { Box, Button, TextField, Typography } from '@mui/material'
 
-const AddItemForm = props => {
-  return (
-    <div>AddWishlistItemForm</div>
-  )
+const AddItemForm = ({handleClose}) => {
+    const [medicineName, setMedicineName] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleMedicineNameChange = (_, name) => {
+        setMedicineName(name)
+    }
+
+    const handleSubmit = () => {
+        
+    }
+
+    return (
+        <>
+            <TextField
+                label="Medicine name"
+                variant="outlined"
+                value={medicineName}
+                onChange={handleMedicineNameChange}
+                required
+            />
+            <Box
+                sx={{
+                    display: "flex",
+                    gap: "1rem",
+                    marginLeft: "auto",
+                    marginRight: 0,
+                }}
+            >
+                <Button variant="outlined" onClick={handleClose}>
+                    Cancel
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={handleSubmit}
+                    disabled={!medicineName || isSubmitting || error}
+                >
+                    Submit
+                </Button>
+            </Box>
+            {error && (
+                <Typography
+                    sx={{
+                        color: "red",
+                        marginLeft: "auto",
+                        marginRight: 0,
+                    }}
+                >
+                    Fail to add this item to the list
+                </Typography>
+            )}
+        </>
+    )
 }
 
 AddItemForm.propTypes = {}
