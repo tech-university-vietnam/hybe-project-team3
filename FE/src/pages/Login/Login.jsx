be37aa374dcf7113314c90f06cc15e5828c396b4
Dung (Yun) Pham <50902849+dungplt242@users.noreply.github.com>
2022-09-08 11:35:50 +0700

    handle access /login when logged in

--- FE/src/pages/Login/Login.jsx
+++ FE/src/pages/Login/Login.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import LoginForm from "components/LoginForm/LoginForm";
+import useAuth from "../../Utils/hooks/auth";
+import { Navigate } from 'react-router-dom';
 
 const Login = () => {
-  return <LoginForm />;
+  const { authed, isLoading } = useAuth();
+
+  return isLoading ? <></> : authed ? <Navigate to="/" replace /> : <LoginForm />;
 };
 
 Login.propTypes = {};
