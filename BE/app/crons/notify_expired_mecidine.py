89d34b522044bae6cdd86c4b2aec8c90e23d6ed1
Al3xDo <hoanghuy9998@gmail.com>
2022-09-09 10:26:24 +0700

    update

--- BE/app/crons/notify_expired_mecidine.py
+++ BE/app/crons/notify_expired_mecidine.py
@@ -72,6 +72,42 @@ def setup_cron(app: FastAPI, debug=False):
         db_repo.db.add_all(noti_list)
         db_repo.db.commit()
 
+    def check_available_meds(meds: [NotificationDTO]):
+        # Get the listed med name
+        # create notis source-order
+
+        near_e: [NotificationDTO] = list(filter(
+            lambda med: med.status == "List", meds))
+        available_med_ids = map(lambda med: med.id, available_meds)
+
+        if debug:
+            available_med_ids = list(available_med_ids)
+            print("%s meds in available_meds" % len(available_med_ids))
+
+        created_notis = db_repo.db.query(NotificationDTO). \
+            where(and_(NotificationDTO.sourcing_type == 'source-order',
+                       NotificationDTO.in_(list(available_med_ids)))).all()
+        created_noti_source_ids = set(map(lambda noti: noti.sourcing_id, created_notis))
+
+        if debug:
+            print("Set created noti: ", created_noti_source_ids)
+
+        # Exclude med existed in noti
+        meds_to_create_noti: [TrackingMedicineDTO] = filter(lambda med: med.id not in created_noti_source_ids,
+                                                            available_meds)
+
+        if debug:
+            meds_to_create_noti = list(meds_to_create_noti)
+            print("%s records in meds_to_create_noti: %s" % (len(meds_to_create_noti), meds_to_create_noti))
+
+        noti_list = []
+        for med in meds_to_create_noti:
+            noti: NotificationDTO = NotificationDTO.from_tracking_medicine(med)
+            noti_list += [noti]
+
+        db_repo.db.add_all(noti_list)
+        db_repo.db.commit()
+
     @app.on_event("startup")
     @repeat_every(seconds=60 * 5)  # 5 mins
     def notify_expired_medicine() -> None:
