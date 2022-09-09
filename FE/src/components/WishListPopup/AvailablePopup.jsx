6316c7c52ccd6ab9ba698267993d84fd2f82d98e
ed-phan <edward_phan@mckinsey.com>
2022-09-09 11:09:47 +0700

    create dummy files

---
+++ FE/src/components/WishListPopup/AvailablePopup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AvailablePopup = (props) => {
+  return <div>AvailablePopup</div>;
+};
+
+AvailablePopup.propTypes = {};
+
+export default AvailablePopup;
